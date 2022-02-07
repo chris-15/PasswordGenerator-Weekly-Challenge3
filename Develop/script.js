d83d7833be30529790148136e6f6079373b11c60
Chris <christopher.sarm15@gmail.com>
2022-02-07 14:47:38 -0500

    update to simpilify prompts- didnt need separate function for prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,43 @@
 // Assignment code here
-//function to get the users choice of length for password
-function getLength () {
-  var length = "";
-  while (length < 8 || length > 128 || isNaN(length) ) {
-    length = parseInt(window.prompt("Please select a password lenght between 8 and 128 characters!"));
-  }
-  // cof
-  if (length > 7 || length < 129 || Number.isInteger(length) ) {
-    var confirmLength = window.confirm("Are you sure you would like to continue with a password length of " + length + "?");
-    if (confirmLength) {
-      window.alert("Your password length is " + length + " characters long!");
-    }
-  }
-
-  console.log("you chose " + length);
-  return length;
-}
-
-
 function generatePassword() {
   console.log("test");
-
   window.alert("Please see following prompts for password criteria. Select length and atleast one character type! ");
 
-  getLength();
+  //prompt to stor password length
+  var passwordLength = parseInt(window.prompt("Please select a password length between 8 and 128 characters!"));
+  //checks to see if lenght is acceptable if not, start over
+  if (passwordLength > 7 && passwordLength < 129 && Number.isInteger(passwordLength)) {
+    window.confirm("Are you sure you would like a password lenght of " + passwordLength + "?");
+  }else {
+    window.alert("Your pasword length is not acceptable. Please try again!");
+    return generatePassword();
+  }
+
+  var isLowerCase = window.confirm("Would you like the password to contain lower case characters?")
+  console.log(isLowerCase); // testing what returns from confirm prompt
+
+  var isUpperCase = window.confirm("Would you like the password to contain upper case characters?")
+  console.log(isUpperCase); // testing what returns from confirm prompt
+
+  var isSpecialCharacter = window.confirm("Would you like the password to contain special characters?")
+  console.log(isSpecialCharacter); // testing what returns from confirm prompt
+
+  //checks to see if atleast one character type was selected, if not start over
+  if ( !isLowerCase && !isUpperCase && !isSpecialCharacter) {
+    window.alert("You need to select atleast one character type");
+    return generatePassword();
+  }
+
+
+
+
+
+
+
+
 
   return "placeholder";
-}
+} // end of generate password functino
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
