79f62eb312a68c1eee810bbf5be65e0b2afd3d9e
Chris <christopher.sarm15@gmail.com>
2022-02-06 20:25:39 -0500

    added function for password length prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,32 @@
 // Assignment code here
+//function to get the users choice of length for password
+function getLength () {
+  var length = "";
+  while (length < 8 || length > 128 || isNaN(length) ) {
+    length = parseInt(window.prompt("Please select a password lenght between 8 and 128 characters!"));
+  }
+  // cof
+  if (length > 7 || length < 129 || Number.isInteger(length) ) {
+    var confirmLength = window.confirm("Are you sure you would like to continue with a password length of " + length + "?");
+    if (confirmLength) {
+      window.alert("Your password length is " + length + " characters long!");
+    }
+  }
 
+  console.log("you chose " + length);
+  return length;
+}
+
+
+function generatePassword() {
+  console.log("test");
+
+  window.alert("Please see following prompts for password criteria. Select length and atleast one character type! ");
+
+  getLength();
+
+  return "placeholder";
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
